9f181f455e1b869424d28406349de4ea387bc921
zhangshanbaoKAKE <13663565840@163.com>
2018-10-19 20:47:37 +0800

    提交testpage

---
+++ testpage/src/main/java/com/example/testpage/TestpageApplication.java
@@ -0,0 +1,12 @@
+package com.example.testpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestpageApplication.class, args);
+    }
+}
